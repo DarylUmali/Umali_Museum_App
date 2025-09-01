8faedad20e8496593c50ffe6b22775b5eb7b6068
ccbc-lab1-pc5 <erelynronald@gmail.com>
2025-09-01 17:29:21 +0800

    commit

---
+++ app/src/main/java/ph/edu/comteq/daryllaboratoryexercise3/ExploreActivity.kt
@@ -0,0 +1,230 @@
+package ph.edu.comteq.daryllaboratoryexercise3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ph.edu.comteq.daryllaboratoryexercise3.ui.theme.DarylLaboratoryExercise3Theme
+
+class ExploreActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DarylLaboratoryExercise3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ExploreScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ExploreScreen(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(horizontal = 16.dp)
+            .statusBarsPadding() // Handle status bar insets
+
+    ) {
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Explore",
+            color = Color.White.copy(alpha = 0.8f),
+            fontFamily = playfairdisplayregular,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Spacer(
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color(0xFFD4AF37))
+
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Text(
+                text = "Upcoming Event",
+                color = Color.White.copy(alpha = 0.8f),
+                fontFamily = optima,
+                fontSize = 30.sp
+
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "Tickets",
+                    color = Color.White.copy(alpha = 0.8f),
+                    fontFamily = optima,
+                    fontSize = 18.sp
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Image(
+                    painter = painterResource(id = R.drawable.chevron_right), // Replace with your arrow icon
+                    contentDescription = "Forward Arrow",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF1A1A1A)),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.renaissance), // Replace with your image
+                    contentDescription = "Renaissance Exhibition",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(350.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column {
+                        Text(
+                            text = "10",
+                            color = Color.White,
+                            fontFamily = optima,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(top = 25.dp)
+                        )
+                        Text(
+                            text = "OCT",
+                            color = Color.White.copy(alpha = 0.8f),
+                            fontFamily = optima,
+                            fontSize = 16.sp
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier.weight(1f) // Take remaining space
+                    ) {
+                        Text(
+                            text = "Renaissance Exhibition",
+                            color = Color.White,
+                            fontFamily = playfairdisplayregular,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(start = 16.dp, bottom = 4.dp)
+
+                        )
+                        Text(
+                            text = "9:00 AM - 6:00 PM",
+                            color = Color.White.copy(alpha = 0.7f),
+                            fontFamily = optima,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+                        )
+                        Text(
+                            text = "Indulge in the rich tapestry of Renaissance art",
+                            style = TextStyle(
+                                textDecoration = TextDecoration.Underline
+                            ),
+                            color = Color(0xFFD4AF37),
+                            fontFamily = optima,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(start = 16.dp,bottom = 8.dp)
+                        )
+                        Text(
+                            text = "+33 (0)1 23 45 67 89",
+                            style = TextStyle(
+                                textDecoration = TextDecoration.Underline
+                            ),
+                            color = Color.White.copy(alpha = 0.7f),
+                            fontFamily = optima,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(start = 16.dp)
+
+                        )
+                    }
+                }
+
+                Button(
+                    onClick = { /* Handle visit gallery action */ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFD4AF37),
+                        contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp), // Rounded only at the bottom
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+
+                ) {
+                    Text(
+                        text = "Visit Gallery",
+                        fontFamily = optima,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExploreScreenPreview() {
+    DarylLaboratoryExercise3Theme {
+        ExploreScreen()
+    }
+}
---
+++ app/src/main/java/ph/edu/comteq/daryllaboratoryexercise3/MainActivity.kt
@@ -0,0 +1,150 @@
+package ph.edu.comteq.daryllaboratoryexercise3
+
+import ads_mobile_sdk.h4
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ph.edu.comteq.daryllaboratoryexercise3.ui.theme.DarylLaboratoryExercise3Theme
+import kotlin.jvm.java
+
+val playfairdisplayregular = FontFamily(
+    Font(R.font.playfairdisplayregular, FontWeight.Normal)
+)
+
+val optima = FontFamily(
+    Font(R.font.optima, FontWeight.Normal)
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DarylLaboratoryExercise3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GalleryScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GalleryScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(64.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Gattery Logo",
+            modifier = Modifier
+                .width(120.dp)
+                .height(120.dp)
+                .padding(bottom = 32.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .width(250.dp)
+                .height(400.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.louvre),
+                contentDescription = "Louvre Museum",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Text(
+                text = "Experience Art",
+                color = Color.White.copy(alpha = 0.8f),
+                fontFamily = playfairdisplayregular,
+                fontSize = 33.sp,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter) // Align text to the bottom center of the Box
+                    .offset(y = (-5).dp) // Adjust this value to control the overlap amount
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp)) // Space after the overlapped section
+
+        Text(
+            text = "We are thrilled to invite you to join us for an extraordinary event that will immerse you in the world of art.",
+            color = Color.White.copy(alpha = 0.8f),
+            textAlign = TextAlign.Center,
+            fontFamily = optima,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            modifier = Modifier.padding(bottom = 16.dp) // Added bottom padding for the description
+        )
+
+        Button(
+            onClick = {
+                val intent = Intent(context, ExploreActivity::class.java)
+                context.startActivity(intent)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFD4AF37),
+                contentColor = Color.Black
+            )
+        ) {
+            Text(
+                text = "Explore Now",
+                fontFamily = optima,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GalleryScreenPreview() {
+    DarylLaboratoryExercise3Theme {
+        GalleryScreen()
+    }
+}
